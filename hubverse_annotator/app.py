ed868fed314f882ccf4b5bcd0f7c58dcfa1d3e4b
AFg6K7h4fhy2 <127630341+AFg6K7h4fhy2@users.noreply.github.com>
2025-04-15 11:21:47 -0400

    model target selection

--- hubverse_annotator/app.py
+++ hubverse_annotator/app.py
@@ -76,20 +76,6 @@ def main() -> None:
     uploaded_file = st.file_uploader(
         "Upload Hubverse File", type=["csv", "parquet"]
     )
-    # two-column layout for reference date and location
-    col1, col2 = st.columns(2)
-    with col1:
-        today = datetime.datetime.today().date()
-        reference_date = st.date_input("Reference Date", value=today)
-    with col2:
-        # get locations from forecasttools, some might be excluded from the
-        # hubverse table, though
-        locations = forecasttools.location_table["long_name"].to_list()
-        location = st.selectbox("Location", locations)
-    # get location abbreviation
-    two_letter_loc_abbr = forecasttools.location_lookup(
-        location_vector=[location], location_format="long_name"
-    )["location_code"].item()
     # load the hubverse data
     if uploaded_file is not None:
         if uploaded_file.name.endswith("parquet"):
@@ -98,16 +84,44 @@ def main() -> None:
             smhub_table = pl.read_csv(uploaded_file)
         logger.info(f"Uploaded file:\n{uploaded_file.name}")
         logger.info(f"Contents\n:{smhub_table}")
+        # two-column layout for reference date and location
+        col1, col2 = st.columns(2)
+        with col1:
+            today = datetime.datetime.today().date()
+            reference_date = st.date_input("Reference Date", value=today)
+        with col2:
+            # get locations from forecasttools, some might be excluded from the
+            # hubverse table, though
+            locations = forecasttools.location_table["long_name"].to_list()
+            location = st.selectbox("Location", locations)
+        # get location abbreviation
+        two_letter_loc_abbr = forecasttools.location_lookup(
+            location_vector=[location], location_format="long_name"
+        )["location_code"].item()
         # get hubverse table by selected location
         smhub_table = smhub_table.filter(
             pl.col("location") == two_letter_loc_abbr
         )
+        # models and targets available
         models_available = smhub_table["model"].unique().to_list()
         selected_models = st.multiselect(
             "Select Models To Plot", options=models_available
         )
+        targets_available = smhub_table["target"].unique().to_list()
+        selected_target = st.selectbox(
+            "Select Targets", options=targets_available
+        )
+        # filter hubverse table by selected models
+        smhub_table = smhub_table.filter(
+            pl.col("model").is_in(selected_models),
+            pl.col("target") == selected_target,
+        )
+
         st.markdown(f"## Forecasts For: {location}")
         st.markdown(f"## Reference Date: {reference_date}")
+
+        # plotting of the selected model, target, location, and reference date
+
         # forecasts annotation section
         st.markdown("#### Forecast A")
         st.selectbox(
@@ -129,6 +143,7 @@ def main() -> None:
             "Status", ["Preferred", "Omitted", "None"], key="status_d"
         )
         st.text_input("Comments", key="comments_d")
+
         # export button
         if st.button("Export forecasts"):
             col1, col2, col3 = st.columns([1, 3, 1])
